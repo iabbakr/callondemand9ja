e19a8dfc7b2d57aaa5391a4ad95bcbc44c62548e
Abubakar Ibrahim <abubakaribrahim@Macs-MacBook-Pro.local>
2024-12-23 18:15:30 +0100

    good

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import Menu from './pages/Menu';
@@ -10,7 +11,7 @@ const App = () => {
     return (
         <Router>
             <div className="app">
-                
+                <Header />
                 <main>
                   <Routes>
                       <Route path="/" element={<Home />} />
