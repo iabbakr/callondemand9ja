7111f77611ac2891a182ba05ef4836c49d84b234
iabbakr <abubakaribrahim603@gmail.com>
2024-12-24 16:07:52 +0100

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,26 @@
 import React from 'react';
+import '../css/Header.css';
 
 const Header = () => {
     return (
         <header className="header">
-            <h1 className='logo'>CallOnDemand</h1>
+            <div className="logo">
+                <h1>CallOnDemand</h1>
+            </div>
+            
             <nav>
-                <a href="/">Home</a>
-                <a href="/menu">Menu</a>
-                <a href="/cart">Cart</a>
-                <a href="/user">User</a>
+                <ul className="nav-links">
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/menu">Menu</a></li>
+                    <li><a href="/cart">Cart</a></li>
+                    <li><a href="/user">User</a></li>  
+                </ul>
             </nav>
+            <div class="menu-toggle">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
         </header>
     );
 };
