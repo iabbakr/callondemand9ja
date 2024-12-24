141d2c731889e5b073a1947f83dace6f701a7588
iabbakr <abubakaribrahim603@gmail.com>
2024-12-24 10:45:01 +0100

    added responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
     return (
         <header className="header">
-            <h1>CallOnDemand</h1>
+            <h1 className='logo'>CallOnDemand</h1>
             <nav>
                 <a href="/">Home</a>
                 <a href="/menu">Menu</a>
