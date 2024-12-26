7ab96706ef09abb61647d117efd13236dc23b56a
iabbakr <abubakaribrahim603@gmail.com>
2024-12-26 16:55:00 +0100

    added c

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 // App.js
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
-import Home from './components/Home/Home';
-import Menu from './components/Menu/Menu';
-import CartPage from './components/CartPage/CartPage';
-import UserPage from './components/UserPage/UserPage';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './components/Home';
+import Menu from './components/Menu';
+import CartPage from './components/CartPage';
+import UserPage from './components/UserPage';
 
 const App = () => {
     const [cart, setCart] = useState([]);
